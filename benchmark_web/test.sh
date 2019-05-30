a4c211a30479871ecd887d2f0a9276f9d0d48753
Ryan Tate <rtate@willistonfinancial.com>
2019-05-29 23:54:08 -0700

    run php first to allow pm2 time to start

--- benchmark_web/test.sh
+++ benchmark_web/test.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo '' > benchmark.log
-for platform in node php
+for platform in php node
     do
     for i in $(seq 1 10)
     do
         n=$(($i * 100))
         php mk_list.php $n
         echo testing $platform with list size $n
-        ab -l -p list.json -T application/json -c $i -n $n http://$platform/ >> benchmark.log
+        ab -l -p list.json -T application/json -c $n -n 10000 http://$platform/ >> benchmark.log
     done
 done
 echo done!
