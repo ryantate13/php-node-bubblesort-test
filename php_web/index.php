7e89d115c6f053323a221820500b381622d851a3
Ryan Tate <rtate@willistonfinancial.com>
2019-05-30 13:05:50 -0700

    fix bug with input stream

--- php_web/index.php
+++ php_web/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once './bubble-sort-v2.php';
-$list = json_decode(file_get_contents('php://stdin'));
+$list = json_decode(file_get_contents('php://input'));
 $start = hrtime(true);
 bubbleSort($list);
 $end = hrtime(true);
